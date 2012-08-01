863036150e2c8cacfb36520d1add88b0401ead88
Keichi Takahashi <keichi.t@me.com>
2012-08-01 16:46:20 +0900

    個別記事表示用のルートを追加

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,10 @@
 exports.routes = function (map) {
-
     // Generic routes. Add all your routes below this line
     // feel free to remove generic routes
     map.root('top#home');
+    map.get('/post/:id', 'post#show');
+
+    // Internal routes
     map.post('/internal/post/create', 'post#create');
     map.post('/internal/post/delete', 'post#delete');
-    map.get('/internal/post/show', 'post#show');
 };
