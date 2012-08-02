5f1637b82f5fb4ddabfbef27ef92e065e2016db2
chibiegg <chibiegg@chibiegg.net>
2012-08-02 13:28:00 +0900

    投稿画面に住所とラベルの入力フォームを追加するプラグイン

---
+++ wordpress/plugins/trads_geoedit.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: Trads GEO Edit
+Plugin URI: http://blog.chibiegg.net/
+Description: Trads GEO Edit
+Version: 0.1 alpha
+Author: chibiegg
+Author URI: http://blog.chibiegg.net/
+License: Undefined
+*/
+
+
+
+function add_geo_edit(){
+	?>
+	
+	<p>ここに入るはず</p>
+	<?php
+
+}
+
+add_action('dbx_post_advanced', 'add_geo_edit');
+
+
+
+?>
