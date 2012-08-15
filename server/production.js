71e8083dd2e8f0774d9d652e1ba52843872434e0
www-data <www-data@cc.chibiegg.net>
2012-08-15 18:56:02 +0900

    production.jsを準備

---
+++ server/production.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+process.env.NODE_ENV = 'production';
+var app = module.exports = require('railway').createServer();
+
+if (!module.parent) {
+    var port = process.env.PORT || 3000
+    app.listen(port);
+    console.log("Railway server listening on port %d within %s environment", port, app.settings.env);
+}
+
