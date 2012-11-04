db047341a978daff6c9bbf11a5c4388af1014765
Keichi Takahashi <keichi.t@me.com>
2012-11-04 23:00:34 +0900

    FacebookのLike!数の取得処理を追加

--- server/public/javascripts/script.js
+++ server/public/javascripts/script.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
     var initWidth = $container.width();
     var initHeight = $container.height();
     var spinner;
+    var root = location.protocol + '//' + location.host;
 
     $('.post-box img').contenthover({
         overlay_background  :   '#000',
@@ -21,32 +22,23 @@ $(document).ready(function() {
         return false;
     });
 
-    $('.meta-favorite').powerTip();
-    $('.meta-view').powerTip();
-    $('.meta-like').powerTip();
-    $('.meta-comments').powerTip();
-    $('#navigation li a').powerTip();
+    $('.meta-favorite, .meta-view, .meta-like, .meta-comments, #navigation li a').powerTip();
 
-    $('.meta-favorite').click(function() {
-        var star = $(this).children('i');
-        var postId = $(this).parents('.post-box').attr('id').replace('post-', '');
-
-        $.ajax({
-            url: '/favorites/switch',
-            data: {
-                id: postId 
-            },
-            dataType: 'json',
-            success: function( data, textStatus, jqXHR ) {
-                if (data.error) {
-                    alert('Failed to register to favorites!');
-                } else {
-                    star.toggleClass('icon-star-empty');
-                    star.toggleClass('icon-star');
-                }
+    $('.title-author .post-title a', $box).each(function() {
+        var url = root + $(this).attr('href');
+        var like = $('.meta-like', $(this).parents('.post-box'));
+        $.getJSON(
+            'https://graph.facebook.com/' + url + '&callback=?',
+            function(data) {
+                var count = data.shares || 0;
+                var title_author = 
+                like.append(String(count));
             }
-        });
+            );
+    });
 
+    $('.meta-view, .meta-like, .meta-comments').click(function(e) {
+        e.preventDefault();
         return false;
     });
 
@@ -60,31 +52,30 @@ $(document).ready(function() {
             $(this).animate({boxShadow: '0 0 20px #000'}, 300);
         }, function() {
             $(this).animate({boxShadow: '0 1px 4px #000'}, 300);
-      });
+        });
 
-    var opts = {
-        lines: 13, // The number of lines to draw
-        length: 7, // The length of each line
-        width: 4, // The line thickness
-        radius: 10, // The radius of the inner circle
-        corners: 1, // Corner roundness (0..1)
-        rotate: 0, // The rotation offset
-        color: '#fff', // #rgb or #rrggbb
-        speed: 1, // Rounds per second
-        trail: 60, // Afterglow percentage
-        shadow: false, // Whether to render a shadow
-        hwaccel: false, // Whether to use hardware acceleration
-        className: 'spinner', // The CSS class to assign to the spinner
-        zIndex: 2e9, // The z-index (defaults to 2000000000)
-        top: 'auto', // Top position relative to parent in px
-        left: 'auto', // Left position relative to parent in px
-      };
-      $('#container').prepend('<div id="loading"></div>');
-      spinner = new Spinner(opts).spin($('#loading')[0]);
-  }
+        var opts = {
+            lines: 13, // The number of lines to draw
+            length: 7, // The length of each line
+            width: 4, // The line thickness
+            radius: 10, // The radius of the inner circle
+            corners: 1, // Corner roundness (0..1)
+            rotate: 0, // The rotation offset
+            color: '#fff', // #rgb or #rrggbb
+            speed: 1, // Rounds per second
+            trail: 60, // Afterglow percentage
+            shadow: false, // Whether to render a shadow
+            hwaccel: false, // Whether to use hardware acceleration
+            className: 'spinner', // The CSS class to assign to the spinner
+            zIndex: 2e9, // The z-index (defaults to 2000000000)
+            top: 'auto', // Top position relative to parent in px
+            left: 'auto', // Left position relative to parent in px
+        };
+        $('#container').prepend('<div id="loading"></div>');
+        spinner = new Spinner(opts).spin($('#loading')[0]);
+    }
 
-  function layout(e) {
-        // $box.css('display', 'inline-block');
+    function layout(e) {
         spinner.stop();
 
         var cw = initWidth || $container.width();
@@ -103,10 +94,33 @@ $(document).ready(function() {
             .animate({
                 'left': Math.round((i % cols) * (w + _m)) + _m,
                 'top': Math.floor(i / cols) * (h + _m)
-        }, s + i * 30, 'easeOutBounce');
-    });
+            }, s + i * 30, 'easeOutBounce');
+        });
 
         initWidth = undefined;
+
+        $('.meta-favorite').click(function() {
+            var star = $(this).children('i');
+            var postId = $(this).parents('.post-box').attr('id').replace('post-', '');
+
+            $.ajax({
+                url: '/favorites/switch',
+                data: {
+                    id: postId 
+                },
+                dataType: 'json',
+                success: function( data, textStatus, jqXHR ) {
+                    if (data.error) {
+                        alert('Failed to register to favorites!');
+                    } else {
+                        star.toggleClass('icon-star-empty');
+                        star.toggleClass('icon-star');
+                    }
+                }
+            });
+
+            return false;
+        });
     }
 
     if ($('#loginlinks').length == 0) {
