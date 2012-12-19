8480729ca51f098405a0cc6d7a21a55527355713
Keichi Takahashi <keichi.t@me.com>
2012-12-19 20:55:11 +0900

    Imageのurlを修正するユーティリティ関数を追加
    httpsをhttpに置換する

--- server/app/models/image.js
+++ server/app/models/image.js
@@ -1,1 +1,20 @@
+var async = require('async');
+
 Image.belongsTo(Post, {as: 'post', foreignKey: 'postId'});
+
+Image.changeToHttp = function(cb) {
+	Image.all(function(err, images) {
+		if (err) {cb(err);}
+
+		async.forEach(
+			images,
+			function(image, c){
+				image.url = image.url.replace(/^https(.*)/, 'http$1');
+				image.save(c);
+			},
+			function(err) {
+				cb(err);
+			}
+		);
+	});
+};
