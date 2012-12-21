289615ad4141b56ed3dc9f556612151abdb79c56
Keichi Takahashi <keichi.t@me.com>
2012-12-21 11:21:02 +0900

    データベースをmongooseアダプタからmongodbアダプタに変更

---
+++ tools/update-from-mongoose-to-mongodb.js
@@ -0,0 +1,4 @@
+db.favoritelists.renameCollections('FavoriteList');
+db.favoriteerealtions.renameCollections('FavoriteRelation');
+db.images.renameCollections('Image');
+db.users.renameCollections('User');
