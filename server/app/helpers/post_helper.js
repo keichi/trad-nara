175901fced3924e76393043c857b050760426485
Keichi Takahashi <keichi.t@me.com>
2012-12-28 13:46:51 +0900

    投稿メタ情報の表示を修正

--- server/app/helpers/post_helper.js
+++ server/app/helpers/post_helper.js
@@ -1,4 +1,12 @@
 module.exports = {
-    uriencode : function(s){return encodeURI(s);},
-    profile_img_url : require('./top_helper').profile_img_url,
+	uriencode : function(s){return encodeURI(s);},
+	profile_img_url : require('./top_helper').profile_img_url,
+	format_date: function(date) {
+		var month_names = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+		var year = date.getFullYear();
+		var month = date.getMonth();
+		var day = date.getDate();
+
+		return month_names[month] + ' ' + day + ', ' + year;
+    }
 };
